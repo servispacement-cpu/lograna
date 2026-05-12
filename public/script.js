da6564f75f02df90cbc15af9867f665353978bf9
Aaron Maller <servispacement@gmail.com>
2026-05-12 19:12:54 +0200

    fisrtcomit

---
+++ public/script.js
@@ -0,0 +1,35 @@
+async function afficherCookies(){
+    const dataCo = await getCo();
+    for(let i = 0; i>dataCo.length; i++){
+    const div = document.createElement("div");
+    div.id = "div";
+    document.getElementById("par").appendChild(div);
+    const Nco = document.createElement("h3");
+    const desc = document.createElement("h3");
+    const prix = document.createElement("h3");
+    Nco.textContent = dataCo.Nco;
+    desc.textContent = dataCo.desc;
+    prix.textContent = dataCo.prix;
+    div.appendChild(Nco);
+    }
+}
+
+afficherCookies();
+
+
+async function getCo(){
+    const url = "lograna.onrender.com/cookies";
+        try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) throw new Error(`Erreur HTTP : ${response.status}`);
+        const data = await response.json();
+        return (data);
+    } catch (error) {
+        console.error('Erreur :', error);
+        throw error;
+    } 
+}
---
+++ server.js
@@ -0,0 +1,70 @@
+import dns from 'dns';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors  from 'cors';
+
+//dns.setDefaultResultOrder('ipv4first');
+
+// Create a new Express.js instance
+const app = express();
+
+ 
+
+// Set up middleware
+
+app.use(express.json());
+
+ 
+
+// Connect to the MongoDB database
+
+mongoose.connect('mongodb://servispacement_db_user:test@ac-wrbrxfi-shard-00-00.r3itdqv.mongodb.net:27017,ac-wrbrxfi-shard-00-01.r3itdqv.mongodb.net:27017,ac-wrbrxfi-shard-00-02.r3itdqv.mongodb.net:27017/Flygana?ssl=true&replicaSet=atlas-10s15u-shard-0&authSource=admin&appName=Cluster0', {
+});
+
+ 
+
+// Define a schema for our data
+
+const Schemac = new mongoose.Schema({
+  Nco: String,
+  desc: String,
+  prix: Number,
+});
+
+ 
+
+// Define a model based on the schema
+
+const Itemc = mongoose.model('Itemb', Schemac);
+
+ 
+
+// Define routes
+app.use(cors()); 
+
+app.post('/cookies', async (req, res) => {
+  try {
+ const item = new Itemc(req.body);
+  await item.save();
+  res.json(item);
+  } catch (error){
+    console.error(error);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
+});
+
+app.get('/cookies', async (req, res) => {
+  const items = await Itemc.find();  
+  res.json(items);
+});
+
+
+app.use(express.static("public"));
+
+// Start the server
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
